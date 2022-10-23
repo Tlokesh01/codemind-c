86a2e874d9ec49049b72f1bc82f4c69c060c2fd1
Lokesh Kumar Talluri <102519379+Tlokesh01@users.noreply.github.com>
2022-10-23 06:39:44 +0530

    #include<bits/stdc++.h> using namespace std;
    int main(){
        int a,b;
        cin >> a >> b;
        for(int i = b; i > 0; i--){
            if(a%i == 0 && b%i == 0){
                cout << i;
                break;
            }
        }
    }

---
+++ GCD_or_HCF.c
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    for(int i = b; i > 0; i--){
+        if(a%i == 0 && b%i == 0){
+            cout << i;
+            break;
+        }
+    }
+}
