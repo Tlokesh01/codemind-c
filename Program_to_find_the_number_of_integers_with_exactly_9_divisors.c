b70028dab372054856f3bf2cc8b10f9f09d55d5a
Lokesh Kumar Talluri <102519379+Tlokesh01@users.noreply.github.com>
2022-11-06 18:16:45 +0530

    #include<bits/stdc++.h> using namespace std;
    bool F(int n){
        int c = 0;
        int i = 1;
        for(int i = 1; i*i <= n; i++){
            if(n%i == 0){
                c+=2;
                if(n/i == i)c--;
            }
        }
        return c==9;
    }
    
    int main(){
        int n;
        cin >> n;
        int ans = 0;
        for(int i = 1; i <= n; i++){
            if(F(i)){
                cout << i << " ";
                ans++;
            }
        }
        cout <<endl<< "Total="<<ans;
    }

---
+++ Program_to_find_the_number_of_integers_with_exactly_9_divisors.c
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool F(int n){
+    int c = 0;
+    int i = 1;
+    for(int i = 1; i*i <= n; i++){
+        if(n%i == 0){
+            c+=2;
+            if(n/i == i)c--;
+        }
+    }
+    return c==9;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int ans = 0;
+    for(int i = 1; i <= n; i++){
+        if(F(i)){
+            cout << i << " ";
+            ans++;
+        }
+    }
+    cout <<endl<< "Total="<<ans;
+}
